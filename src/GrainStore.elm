e9b99067b4b2ac772287cca75c3c644e668dd62c
Jigar Gosar <jigar.gosar@gmail.com>
2018-12-08 16:40:27 +0530

    GrainStore

--- src/GrainStore.elm
+++ src/GrainStore.elm
@@ -1,12 +1,16 @@
-module GrainStore exposing (GrainStore)
+module GrainStore exposing (GrainStore, decodeString)
 
+import DecodeX exposing (Encoder)
 import Dict exposing (Dict)
 import Grain exposing (Grain)
 import GrainId exposing (GrainId)
+import Json.Decode as D exposing (Decoder)
+import Json.Decode.Pipeline exposing (required)
+import Json.Encode as E
 
 
 type alias Model =
-    { lookup : Dict GrainId Grain
+    { items : List Grain
     }
 
 
@@ -20,3 +24,21 @@ unwrap (GrainStore model) =
 
 map fn =
     unwrap >> fn >> GrainStore
+
+
+encoder : Encoder GrainStore
+encoder (GrainStore model) =
+    E.object
+        [ ( "items", E.list Grain.encoder model.items )
+        ]
+
+
+decoder : Decoder GrainStore
+decoder =
+    DecodeX.start Model
+        |> required "items" (D.list Grain.decoder)
+        |> D.map GrainStore
+
+
+decodeString str =
+    D.decodeString decoder str
