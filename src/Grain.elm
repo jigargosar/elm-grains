bca26920b19a9155a2042694409d5fbc6fee3ec4
Jigar Gosar <jigar.gosar@gmail.com>
2018-12-09 19:21:50 +0530

    clean up old code

--- src/Grain.elm
+++ src/Grain.elm
@@ -7,14 +7,32 @@ type GrainIdTag
     = GrainIdTag
 
 
+type alias GrainId =
+    Tagged GrainTag String
+
+
 type GrainTag
     = GrainTag
 
 
 type alias Internal =
-    { id : Tagged GrainIdTag String
+    { id : GrainId
+    , title : String
     }
 
 
 type alias Grain =
     Tagged GrainTag Internal
+
+
+mockInit : String -> Grain
+mockInit title =
+    tag { id = tag title, title = title }
+
+
+mockList : List Grain
+mockList =
+    [ "Line 1"
+    , "Line 2"
+    ]
+        |> List.map mockInit
