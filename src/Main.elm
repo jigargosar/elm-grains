2351328b64632826cfb9b14553849f80bbf2919e
Jigar Gosar <jigar.gosar@gmail.com>
2018-12-11 19:04:18 +0530

    Grain View

--- src/Main.elm
+++ src/Main.elm
@@ -278,7 +278,7 @@ viewFab =
         , Css.boxShadow4 (px 1) (px 1) (px 8) (blackAlpha 0.5)
         , CS.p space2
         ]
-        []
+        [ onClick AddNewClicked ]
         [ CssIcons.viewColorWhite CssIcons.add ]
 
 
