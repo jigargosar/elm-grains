9a4d0d67f7fa3ace379d856dfeea47bc746361c2
Jigar Gosar <jigar.gosar@gmail.com>
2018-12-13 12:43:28 +0530

    server sync

---
+++ src/fire.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase/app'
+
+const config = {
+  apiKey: 'AIzaSyBVS1Tx23pScQz9w4ZDTGh307mqkCRy2Bw',
+  authDomain: 'not-now-142808.firebaseapp.com',
+  databaseURL: 'https://not-now-142808.firebaseio.com',
+  projectId: 'not-now-142808',
+  storageBucket: 'not-now-142808.appspot.com',
+  messagingSenderId: '476064436883',
+}
+firebase.initializeApp(config)
