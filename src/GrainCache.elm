b6c7802e00325823ef2658d1fb4eb5a87239a1d1
Jigar Gosar <jigar.gosar@gmail.com>
2018-12-21 17:34:04 +0530

    Grain Cache

--- src/GrainCache.elm
+++ src/GrainCache.elm
@@ -27,62 +27,22 @@ import Random.Pipeline as Random
 import RandomX
 import Return exposing (Return)
 import Return3 as R3 exposing (Return3F)
-import Saved exposing (Saved)
-
-
-type alias SavedGrain =
-    Saved Grain
-
-
-savedGrainId : SavedGrain -> GrainId
-savedGrainId =
-    Saved.value >> Grain.id
-
-
-savedGrainDecoder : Decoder SavedGrain
-savedGrainDecoder =
-    let
-        decoderWithValue : Grain -> Decoder SavedGrain
-        decoderWithValue value =
-            let
-                saved : SavedGrain
-                saved =
-                    Saved.new value
-            in
-            D.maybe (D.field "initial" Grain.decoder)
-                |> D.map (Maybe.unwrap identity Saved.setSaved >> callWith saved)
-    in
-    D.field "value" Grain.decoder
-        |> D.andThen decoderWithValue
-
-
-savedGrainEncoder : SavedGrain -> Value
-savedGrainEncoder model =
-    if Saved.saved model then
-        E.object
-            [ ( "value", Grain.encoder (Saved.value model) )
-            ]
-
-    else
-        E.object
-            [ ( "initial", Grain.encoder (Saved.discard model |> Saved.value) )
-            , ( "value", Grain.encoder (Saved.value model) )
-            ]
+import SavedGrain exposing (SavedGrain)
 
 
 type alias GrainCache =
-    GrainIdLookup (Saved Grain)
+    GrainIdLookup SavedGrain
 
 
 decoder : Decoder GrainCache
 decoder =
-    D.list savedGrainDecoder
-        |> D.map (GrainIdLookup.fromList savedGrainId)
+    D.list SavedGrain.decoder
+        |> D.map (GrainIdLookup.fromList SavedGrain.id)
 
 
 encoder : GrainCache -> Value
 encoder =
-    GrainIdLookup.toList >> E.list savedGrainEncoder
+    GrainIdLookup.toList >> E.list SavedGrain.encoder
 
 
 empty : GrainCache
@@ -93,8 +53,8 @@ empty =
 setSaved : Grain -> GrainCache -> GrainCache
 setSaved grain =
     GrainIdLookup.update (Grain.id grain)
-        (Maybe.map (Saved.setSaved grain)
-            >> Maybe.orElseLazy (\_ -> Just <| Saved.new grain)
+        (Maybe.map (SavedGrain.setSaved grain)
+            >> Maybe.orElseLazy (\_ -> Just <| SavedGrain.new grain)
         )
 
 
--- src/SavedGrain.elm
+++ src/SavedGrain.elm
@@ -1,6 +1,17 @@
-module SavedGrain exposing (SavedGrain, setSaved, value)
+module SavedGrain exposing
+    ( SavedGrain
+    , change
+    , decoder
+    , discard
+    , encoder
+    , id
+    , new
+    , saved
+    , setSaved
+    , value
+    )
 
-import BasicsX exposing (eqs, flip)
+import BasicsX exposing (callWith, eqs, flip)
 import Compare
 import DecodeX exposing (Encoder)
 import Grain exposing (Grain)
@@ -16,6 +27,11 @@ import TimeX
 
 type SavedGrain
     = SavedGrain Grain Grain
+
+
+id : SavedGrain -> GrainId
+id (SavedGrain _ latest) =
+    Grain.id latest
 
 
 new : Grain -> SavedGrain
@@ -64,3 +80,22 @@ change fn (SavedGrain initial latest) =
                 initial
     in
     SavedGrain newInitial newLatest
+
+
+init initial latest =
+    SavedGrain initial latest
+
+
+decoder : Decoder SavedGrain
+decoder =
+    D.map2 init
+        (D.field "initial" Grain.decoder)
+        (D.field "latest" Grain.decoder)
+
+
+encoder : SavedGrain -> Value
+encoder (SavedGrain initial latest) =
+    E.object
+        [ ( "initial", Grain.encoder initial )
+        , ( "latest", Grain.encoder latest )
+        ]
