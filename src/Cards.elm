48246e5808d84f9b6469cb542e84238669cd0c42
Jigar Gosar <jigar.gosar@gmail.com>
2018-12-22 16:36:57 +0530

    UI Cards: Button

--- src/Cards.elm
+++ src/Cards.elm
@@ -33,7 +33,19 @@ viewDocument _ =
 
 
 viewPage =
-    div [] [ viewFlatButton1, viewBorderButton1 ]
+    div
+        [ css
+            [ Css.width <| rem 30
+            , CS.max_w_screen
+            , Css.margin Css.auto
+            , Css.boxShadow4
+                (px 0)
+                (px 0)
+                (px 50)
+                (CS.fromMaterialColor MaterialColor.grey500)
+            ]
+        ]
+        [ div [ css [ CS.pa (rem 1) ] ] [ viewFlatButton1, viewBorderButton1 ] ]
 
 
 main : Program () () ()
