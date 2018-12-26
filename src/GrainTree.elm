89c04cbffe94ca13bee7f017b21c6cab6f7728bc
Jigar Gosar <jigar.gosar@gmail.com>
2018-12-26 16:51:37 +0530

    GrainTree

--- src/GrainTree.elm
+++ src/GrainTree.elm
@@ -1,36 +1,38 @@
-module GrainTree exposing (Forest, GrainTree, forest, tree)
+module GrainTree exposing (Forest, GrainTree)
 
 import Grain exposing (Grain)
 import GrainCache exposing (GrainCache)
 import GrainId exposing (GrainId)
+import Tree
 
 
 type alias Forest =
     List GrainTree
 
 
-type GrainTree
-    = Tree Grain Forest
+type alias GrainTree =
+    Tree.Tree Grain
 
 
-type alias Path =
-    List GrainId
 
-
-forest : GrainCache -> Forest
-forest grainCache =
-    let
-        rootGrains =
-            GrainCache.rootGrains grainCache
-    in
-    rootGrains |> List.map (tree grainCache)
-
-
-tree : GrainCache -> Grain -> GrainTree
-tree grainCache grain =
-    let
-        newForest =
-            GrainCache.childGrains grain grainCache
-                |> List.map (tree grainCache)
-    in
-    Tree grain newForest
+--type alias Path =
+--    List GrainId
+--
+--
+--forest : GrainCache -> Forest
+--forest grainCache =
+--    let
+--        rootGrains =
+--            GrainCache.rootGrains grainCache
+--    in
+--    rootGrains |> List.map (tree grainCache)
+--
+--
+--tree : GrainCache -> Grain -> GrainTree
+--tree grainCache grain =
+--    let
+--        newForest =
+--            GrainCache.childGrains grain grainCache
+--                |> List.map (tree grainCache)
+--    in
+--    Tree grain newForest
