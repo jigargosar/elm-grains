4a584519acd9356cd2d560310cb9697f571d7e0e
Jigar Gosar <jigar.gosar@gmail.com>
2018-12-27 12:52:58 +0530

    GrainTreeView

--- src/GrainTreeView.elm
+++ src/GrainTreeView.elm
@@ -10,7 +10,16 @@ import CssLayout exposing (flexCol, flexRow)
 import CssShorthand as CS
 import CssTheme exposing (black80, blackAlpha, space1, space2, space4, white)
 import EventX
-import Html.Styled exposing (Html, button, div, input, styled, text, textarea)
+import Html.Styled
+    exposing
+        ( Html
+        , button
+        , div
+        , input
+        , styled
+        , text
+        , textarea
+        )
 import Html.Styled.Attributes exposing (autocomplete, class, css, id, rows, tabindex, value)
 import Html.Styled.Events exposing (onBlur, onClick, onFocus, onInput)
 
