5a2a23078f513fe80f761d36fd21ab782ea894c7
Jigar Gosar <jigar.gosar@gmail.com>
2018-12-28 14:35:26 +0530

    Dat like Gui

--- src/DatGui.elm
+++ src/DatGui.elm
@@ -1,6 +1,40 @@
 module DatGui exposing (Field(..))
 
+import Css exposing (num, pct, px, rem, vh, vw, zero)
+import CssAttrX exposing (attrIf)
+import CssElements
+import CssEventX
+import CssHtml
+import CssIcons
+import CssLayout exposing (flexCol, flexRow)
+import CssShorthand as CS
+import CssTheme exposing (black80, blackAlpha, space1, space2, space4, white)
+import EventX
+import Html.Styled
+    exposing
+        ( Html
+        , button
+        , div
+        , input
+        , styled
+        , text
+        , textarea
+        )
+import Html.Styled.Attributes exposing (autocomplete, class, css, id, rows, tabindex, value)
+import Html.Styled.Events
+    exposing
+        ( onBlur
+        , onClick
+        , onDoubleClick
+        , onFocus
+        , onInput
+        )
+
 
 type Field
     = Integer Int
     | Boolean Bool
+
+
+view fields =
+    div [] [ text "fields" ]
