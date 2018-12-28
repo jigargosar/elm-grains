28405e8a27b53ed546c2dcfe752a4d8e19f4157d
Jigar Gosar <jigar.gosar@gmail.com>
2018-12-28 15:12:50 +0530

    Dat like Gui

--- src/DatGui.elm
+++ src/DatGui.elm
@@ -9,10 +9,27 @@ import CssHtml
 import CssIcons
 import CssLayout exposing (flexCol, flexRow)
 import CssShorthand as CS
-import CssTheme exposing (black80, blackAlpha, space1, space2, space4, white)
+import CssTheme
+    exposing
+        ( black80
+        , blackAlpha
+        , space1
+        , space2
+        , space4
+        , white
+        )
 import EventX
 import Html.Styled exposing (Html, button, div, dt, input, styled, text, textarea)
-import Html.Styled.Attributes exposing (autocomplete, class, css, id, rows, tabindex, value)
+import Html.Styled.Attributes
+    exposing
+        ( autocomplete
+        , class
+        , css
+        , id
+        , rows
+        , tabindex
+        , value
+        )
 import Html.Styled.Events
     exposing
         ( onBlur
