ad61894fb04a3caac09281b6f4682e60144eaa83
Jigar Gosar <jigar.gosar@gmail.com>
2018-12-28 20:33:40 +0530

    elm worker

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,9 +1,33 @@
 const path = require('path')
 
 module.exports = {
-  entry: 'src/worker.js',
+  entry: './src/worker.js',
   output: {
-    path: 'dist',
+    path: path.resolve(__dirname, '../dist'),
     filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        loader: 'file-loader?name=[name].[ext]',
+      },
+      {
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        // This is what you need in your own work
+        loader: 'elm-webpack-loader',
+        // loader: '../index.js',
+        options: {
+          debug: true,
+        },
+      },
+    ],
+  },
+
+  devServer: {
+    inline: true,
+    stats: 'errors-only',
   },
 }
--- src/worker.js
+++ src/worker.js
@@ -1,16 +1,11 @@
 // noinspection ES6CheckImport
 import { Elm } from './Worker.elm'
 import { setElmAppPortSubscriptions } from './elm-app'
-import { jsonCacheGetOr } from './json-cache'
 
 // noinspection JSUnresolvedVariable
 const app = Elm.Worker.init({
-  node: document.getElementById('root'),
   flags: {
     now: Date.now(),
-    windowSize: { width: window.innerWidth, height: window.innerHeight },
-    grainCache: jsonCacheGetOr([], 'grainCache'),
-    url: document.URL,
   },
 })
 
