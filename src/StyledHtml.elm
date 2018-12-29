95da7fe368ded3cb0c5cb09b75f83bac5d8074c3
Jigar Gosar <jigar.gosar@gmail.com>
2018-12-29 15:26:22 +0530

    Extract View

---
+++ src/StyledHtml.elm
@@ -0,0 +1,7 @@
+module StyledHtml exposing (Html)
+
+import Html.Styled
+
+
+type alias Html msg =
+    Html.Styled.Html msg
--- src/Toast.elm
+++ src/Toast.elm
@@ -7,53 +7,14 @@ module Toast exposing
     , viewContent
     )
 
-import BasicsX exposing (..)
-import Browser
-import Browser.Dom
-import Browser.Events
-import BrowserX exposing (WindowSize)
-import Color
-import Css exposing (em, num, pct, px)
-import CssElements exposing (..)
-import CssElevation exposing (elevation)
-import CssHtml exposing (viewIf, viewIfLazy)
-import CssIcons exposing (view)
-import CssLayout exposing (flexCol, flexRow)
+import Css exposing (pct, px)
+import CssHtml
+import CssLayout exposing (flexRow)
 import CssShorthand as CS
-import CssTheme exposing (black80, space2, space4, space8, white)
-import DecodeX exposing (DecodeResult)
-import Either exposing (Either(..))
-import EventX exposing (onKeyDownPD)
-import Grain exposing (Grain)
-import GrainListView exposing (GrainListView)
-import GrainStore exposing (GrainStore)
-import HotKey as K exposing (SoftKey(..))
-import Html.Styled as Html exposing (..)
-import Html.Styled.Attributes as SA exposing (..)
-import Html.Styled.Events as SE exposing (onBlur, onClick, onFocus, onInput, onSubmit)
-import Html.Styled.Keyed
-import Json.Decode as D
-import Json.Decode.Pipeline exposing (optional)
-import Json.Encode as E exposing (Value)
-import List.Extra as List
-import ListIndex exposing (ListIndex)
-import Material.Icons.Action as MIcons
-import Material.Icons.Alert as MIcons
-import Material.Icons.Content as MIcons
-import Material.Icons.Editor as MIcons
-import Material.Icons.Navigation as MIcons
-import Material.Icons.Toggle as MIcons
-import Maybe.Extra as Maybe
-import Port
-import Random exposing (Generator, Seed)
-import Random.Pipeline as Random
-import RandomId
-import Result.Extra as Result
-import Return exposing (Return)
-import Return3 as R3 exposing (Return3F)
-import Tagged
-import Task
-import Tuple exposing (mapFirst)
+import CssTheme exposing (space4)
+import Html.Styled as H exposing (text)
+import Html.Styled.Events exposing (onClick)
+import StyledHtml exposing (Html)
 
 
 type alias Toast =
@@ -74,7 +35,7 @@ show title model =
 
 view : msg -> Toast -> Html msg
 view dismissMsg toast =
-    viewIfLazy toast.visible
+    CssHtml.viewIfLazy toast.visible
         (\_ -> viewContent dismissMsg toast.title)
 
 
@@ -84,18 +45,18 @@ viewContent dismissMsg title =
         , Css.bottom <| space4
         , Css.minWidth <| px 150
         , Css.maxWidth <| pct 80
-        , Css.backgroundColor black80
-        , Css.color white
+        , CS.bgBlack80
+        , CS.colorWhite
         ]
         []
         [ flexRow
-            [ Css.flexGrow <| num 1
-            , Css.justifyContent Css.center
-            , CS.pa space2
+            [ CS.flexGrow1
+            , CS.justifyCenter
+            , CS.pa2
             ]
             []
             [ text title ]
-        , flexRow [ CS.pa space2 ]
-            [ class "pointer", onClick dismissMsg ]
+        , flexRow [ CS.pa2, CS.pointer ]
+            [ onClick dismissMsg ]
             [ text "X" ]
         ]
